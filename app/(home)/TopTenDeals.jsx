3c90f6a1e3e92d73c64cbc0df2563259beee812e
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-05-14 22:16:44 +0530

    added Carousel

--- app/(home)/TopTenDeals.jsx
+++ app/(home)/TopTenDeals.jsx
@@ -1,7 +1,14 @@
-"use client"
-import * as React from "react"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -9,72 +16,70 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import { MdUpdate } from "react-icons/md";
-import { BackgroundGradient } from "@/components/ui/background-gradient"
-import Image from "next/image"
-import { dealsData } from "@/utils/data/TopTenDeals"
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import Image from "next/image";
+import { dealsData } from "@/utils/data/TopTenDeals";
 
 export default function TopTenDeals() {
     return (
         <>
             <div className="flex flex-col items-center justify-center mt-10">
-
-                <BackgroundGradient className=" px-20 py-3 rounded-lg flex items-center justify-center overflow-hidden">
+                <BackgroundGradient className="px-20 py-3 rounded-lg flex items-center justify-center overflow-hidden">
                     <h1 className="text-center text-white font-bold text-[20px]">TOP DEALS TODAY</h1>
                 </BackgroundGradient>
-                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-10">
-                    {
-                        dealsData.map((item) => (
-                            <Card key={item.id} className="w-[330px] ">
-                                <CardHeader>
-                                    <Image
-                                        src={`${item.imageSrc}`}
-                                        alt={`${item.name}`}
-                                        height="300"
-                                        width="300"
-                                        className="object-contain rounded-lg"
-                                    />
 
-                                </CardHeader>
-                                <CardContent>
-                                    <CardDescription>{item.name}</CardDescription>
-                                    <div className="flex justify-between mt-4">
-                                        <div className="flex flex-col items-start">
-                                            <p className="text-green-500">₹{item.discountedPrice}</p>
-                                            <p className="text-red-500 line-through">{item.originalPrice}</p>
-                                        </div>
-                                        <div className="flex flex-col items-end">
-                                            <p className="text-green-500">{item.discountPercentage}% OFF</p>
-                                            <div className="flex items-center gap-1 mt-1">
-                                                <MdUpdate className="text-[12px]" />
-                                                <p className="text-[12px] "> {item.timeAgo}</p>
+                <div className="w-full max-w-6xl mt-10 px-4 ">
+                    <Carousel
+                        opts={{
+                            align: "start",
+                            loop: false,
+                        }}
+                        className="w-full"
+                    >
+                        <CarouselContent>
+                            {dealsData.map((item) => (
+                                <CarouselItem key={item.id} className="md:basis-1/2 lg:basis-1/4 pl-4">
+                                    <Card className="w-full dark:bg-[#141a1d] bg-[#e7dfea]  max-w-[330px] mx-auto">
+                                        <CardHeader>
+                                            <Image
+                                                src={`${item.imageSrc}`}
+                                                alt={`${item.name}`}
+                                                height="300"
+                                                width="300"
+                                                className="object-contain rounded-lg"
+                                            />
+                                        </CardHeader>
+                                        <CardContent>
+                                            <CardDescription>{item.name}</CardDescription>
+                                            <div className="flex justify-between mt-4">
+                                                <div className="flex flex-col items-start">
+                                                    <p className="text-green-500">₹{item.discountedPrice}</p>
+                                                    <p className="text-red-500 line-through">{item.originalPrice}</p>
+                                                </div>
+                                                <div className="flex flex-col items-end">
+                                                    <p className="text-green-500">{item.discountPercentage}% OFF</p>
+                                                    <div className="flex items-center gap-1 mt-1">
+                                                        <MdUpdate className="text-[12px]" />
+                                                        <p className="text-[12px]">{item.timeAgo}</p>
+                                                    </div>
+                                                </div>
                                             </div>
-
-                                        </div>
-                                    </div>
-                                </CardContent>
-
-                                <CardFooter className="flex justify-between">
-                                    <Button variant="outline">PRICE HISTORY</Button>
-                                    <Button>BUY</Button>
-                                </CardFooter>
-                            </Card>
-                        ))
-                    }
-
-
-                </div>
-                <button className="p-[3px] relative my-7">
-                    <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-                    <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                        For More Product Deals
-                    </div>
-                </button>
-
+                                        </CardContent>
+                                        <CardFooter className="flex justify-between">
+                                            <Button variant="outline">PRICE HISTORY</Button>
+                                            <Button>BUY</Button>
+                                        </CardFooter>
+                                    </Card>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious className="dark:bg-[#141a1d] bg-[#e7dfea] hidden sm:flex  " />
+                        <CarouselNext className="dark:bg-[#141a1d] bg-[#e7dfea] hidden sm:flex " />
+                    </Carousel>
+                </div>  
             </div>
-
-
         </>
     );
 }
